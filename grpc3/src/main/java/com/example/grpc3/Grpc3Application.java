45b5d9a16c304a2743771262d9c1bf5d5ef799c7
Basmaelkak <kak.basma08@gmail.com>
2024-12-10 21:22:17 +0100

    Premier commit

---
+++ grpc3/src/main/java/com/example/grpc3/Grpc3Application.java
@@ -0,0 +1,13 @@
+package com.example.grpc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grpc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grpc3Application.class, args);
+	}
+
+}
---
+++ grpc3/src/test/java/com/example/grpc3/Grpc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grpc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
